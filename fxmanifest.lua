02ad820b6fd2ae45ae782d693a885aea442838d5
MrSethu00 <100898307+MrSethu00@users.noreply.github.com>
2026-07-09 22:28:44 +0400

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'sethu' 
+
+loadscreen 'html/index.html'
+loadscreen_cursor 'no'
+
+files {
+    'html/index.html',
+    'html/css/style.css',
+    'html/js/loading.js',
+    'html/assets/images/logo.png',
+}
